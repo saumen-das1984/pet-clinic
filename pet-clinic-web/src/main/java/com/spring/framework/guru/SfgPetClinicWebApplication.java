982ddc4f9a93fbbd32173be7f7b07c3e71981368
saumen das <masterblastersaumen@gmail.com>
2023-02-20 20:14:43 +0530

    Main Class Added

---
+++ pet-clinic-web/src/main/java/com/spring/framework/guru/SfgPetClinicWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.framework.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicWebApplication.class, args);
+	}
+
+}
