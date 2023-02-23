4e12bc92747fe01a0fe004fef0a2b9427b88795e
saumen das <masterblastersaumen@gmail.com>
2023-02-23 20:11:47 +0530

    List of Owners Implementation

--- pet-clinic-web/src/main/java/com/spring/framework/guru/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/spring/framework/guru/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package com.spring.framework.guru.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.framework.guru.service.OwnerService;
+
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 	
-	@RequestMapping({ "/owners", "/owners/index", "/owners/index.html" })
-	public String index() {
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String index(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 
