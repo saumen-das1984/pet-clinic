9d396a0a0436c87d4b26b8ffa8c4780ed7dd1dab
saumen das <masterblastersaumen@gmail.com>
2023-02-26 20:52:30 +0530

    Create Speciality Map Service

---
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/service/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.spring.framework.guru.service;
+
+import com.spring.framework.guru.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/service/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.spring.framework.guru.service.map;
+
+import java.util.Set;
+
+import com.spring.framework.guru.model.Speciality;
+import com.spring.framework.guru.service.SpecialityService;
+
+public class SpecialityMapService extends  AbstractMapService<Speciality, Long> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality speciality) {
+		return super.save(speciality);
+	}
+
+	@Override
+	public void delete(Speciality speciality) {
+		super.delete(speciality);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
