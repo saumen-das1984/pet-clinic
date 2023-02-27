210aa2a69315b2a101ad3ac4ec4c9b81684230ea
saumen das <masterblastersaumen@gmail.com>
2023-02-27 19:28:15 +0530

    Create Base Entity

--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/BaseEntity.java
@@ -2,15 +2,23 @@ package com.spring.framework.guru.model;
 
 import java.io.Serializable;
 
+import javax.persistence.MappedSuperclass;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 
 }
