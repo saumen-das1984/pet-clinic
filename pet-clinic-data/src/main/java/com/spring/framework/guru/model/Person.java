cb183e2f65b28577098ec60dcb1f7d8e06725fa7
saumen das <masterblastersaumen@gmail.com>
2023-02-27 19:48:04 +0530

    Convers Owners to JPA Entities

--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/Person.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/Person.java
@@ -1,15 +1,22 @@
 package com.spring.framework.guru.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class Person extends BaseEntity{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
 
 }
--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/PetType.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/PetType.java
@@ -1,14 +1,21 @@
 package com.spring.framework.guru.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "pet_types")
 public class PetType extends BaseEntity{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name = "name")
 	private String name;
 
 }
