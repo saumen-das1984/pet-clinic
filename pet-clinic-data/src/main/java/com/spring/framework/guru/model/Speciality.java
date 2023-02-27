6bafc1ec6180fc36c4901db7162f080192aa1917
saumen das <masterblastersaumen@gmail.com>
2023-02-27 19:56:52 +0530

    Converts Vets to JPA Entities

--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/Speciality.java
@@ -1,13 +1,21 @@
 package com.spring.framework.guru.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "description")
 	private String description;
 
 }
--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/Vet.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/Vet.java
@@ -3,15 +3,26 @@ package com.spring.framework.guru.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
 	private static final long serialVersionUID = 1L;
 	
+	@ManyToMany
+	@JoinTable(name = "vet_speacialities", joinColumns = @JoinColumn(name="vet_id"),
+	inverseJoinColumns = @JoinColumn(name="speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
 }
