b8c39e22e15bf74ccc0db6483bc0a831b0bccb6f
saumen das <masterblastersaumen@gmail.com>
2023-02-27 20:04:56 +0530

    Create Visit entity

--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/Pet.java
@@ -1,11 +1,15 @@
 package com.spring.framework.guru.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -32,5 +36,8 @@ public class Pet extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name="owner_id")
 	private Owner owner;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 	
 }
--- pet-clinic-data/src/main/java/com/spring/framework/guru/model/Visit.java
+++ pet-clinic-data/src/main/java/com/spring/framework/guru/model/Visit.java
@@ -2,17 +2,31 @@ package com.spring.framework.guru.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
 
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 }
