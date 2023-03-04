e203d2e9569ab8647b0eb1f1246e996b96327690
saumen das <masterblastersaumen@gmail.com>
2023-03-04 21:09:22 +0530

    Convert to JUnit5

---
+++ pet-clinic-web/src/test/java/com/spring/framework/guru/SfgPetClinicWebApplicationTest.java
@@ -0,0 +1,17 @@
+package com.spring.framework.guru;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class SfgPetClinicWebApplicationTest {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+	
+	
+
+}
