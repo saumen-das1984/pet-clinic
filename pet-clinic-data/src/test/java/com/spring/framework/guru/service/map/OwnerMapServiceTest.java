6c978546fb24ffc9161d7f5e05098cfb09dffc47
saumen das <masterblastersaumen@gmail.com>
2023-03-06 19:38:25 +0530

    CRUD Test for Owner Map Service

--- pet-clinic-data/src/test/java/com/spring/framework/guru/service/map/OwnerMapServiceTest.java
+++ pet-clinic-data/src/test/java/com/spring/framework/guru/service/map/OwnerMapServiceTest.java
@@ -2,7 +2,6 @@ package com.spring.framework.guru.service.map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Set;
 
