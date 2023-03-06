5245a20280840eaac2dd08fce7fdc3946557e971
saumen das <masterblastersaumen@gmail.com>
2023-03-06 19:37:48 +0530

    CRUD Tests for Owner Map Service

---
+++ pet-clinic-data/src/test/java/com/spring/framework/guru/service/map/OwnerMapServiceTest.java
@@ -0,0 +1,75 @@
+package com.spring.framework.guru.service.map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.spring.framework.guru.model.Owner;
+
+class OwnerMapServiceTest {
+	
+	OwnerMapService ownerMapService;
+	
+	private Long id = 1L;
+	private String lastName = "Smith";
+
+	@BeforeEach
+	void setUp() {
+		ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+		ownerMapService.save(Owner.builder().id(id).lastName(lastName).build());
+	}
+
+	@Test
+	void testFindAll() {
+		Set<Owner> owners = ownerMapService.findAll();
+		assertEquals(1, owners.size());
+	}
+
+	@Test
+	void testFindByIdLong() {
+		Owner owner = ownerMapService.findById(id);
+		assertEquals(1, owner.getId());
+	}
+
+	@Test
+	void testSaveOwnerWithId() {
+		Long ownerId = 2L;
+		Owner owner2 = Owner.builder().id(ownerId).build();
+		Owner savedOwner = ownerMapService.save(owner2);
+		
+		assertEquals(ownerId, savedOwner.getId());
+	}
+	
+	@Test
+	void testSaveOwnerWithOutId() {
+		Owner owner2 = Owner.builder().build();
+		Owner savedOwner = ownerMapService.save(owner2);
+		assertNotNull(savedOwner);
+		assertNotNull(savedOwner.getId());
+	}
+
+	@Test
+	void testDeleteOwner() {
+		ownerMapService.delete(ownerMapService.findById(id));
+		assertEquals(0, ownerMapService.findAll().size());
+	}
+
+	@Test
+	void testDeleteByIdLong() {
+		ownerMapService.deleteById(id);
+		assertEquals(0, ownerMapService.findAll().size());
+	}
+
+	@Test
+	void testFindByLastName() {
+		Owner smith = ownerMapService.findByLastName(lastName);
+		assertNotNull(smith);
+		assertNotNull(smith.getId());
+	}
+
+}
