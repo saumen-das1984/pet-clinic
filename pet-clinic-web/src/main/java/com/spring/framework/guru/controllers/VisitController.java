8f20b765139e48f0965b9b0640343a56f53c239c
saumen das <masterblastersaumen@gmail.com>
2023-04-07 20:43:37 +0530

    Create or Update Visit

---
+++ pet-clinic-web/src/main/java/com/spring/framework/guru/controllers/VisitController.java
@@ -0,0 +1,84 @@
+package com.spring.framework.guru.controllers;
+
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.spring.framework.guru.model.Pet;
+import com.spring.framework.guru.model.Visit;
+import com.spring.framework.guru.service.PetService;
+import com.spring.framework.guru.service.VisitService;
+
+@Controller
+public class VisitController {
+	
+	private static final String VIEWS_VISITS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateVisitForm";
+	
+	private final VisitService visitService;
+    private final PetService petService;
+    
+	public VisitController(VisitService visitService, PetService petService) {
+		this.visitService = visitService;
+		this.petService = petService;
+	}
+    
+	@InitBinder
+    public void dataBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
+
+    /**
+     * Called before each and every @RequestMapping annotated method.
+     * 2 goals:
+     * - Make sure we always have fresh data
+     * - Since we do not use the session scope, make sure that Pet object always has an id
+     * (Even though id is not part of the form fields)
+     *
+     * @param petId
+     * @return Pet
+     */
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Model model) {
+        return VIEWS_VISITS_CREATE_OR_UPDATE_FORM;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_VISITS_CREATE_OR_UPDATE_FORM;
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
